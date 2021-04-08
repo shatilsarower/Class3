91cfa9ffd9bffb1bfdda36f82af68e2db0eb7ba1
shatilsarower <shatid12@gmail.com>
2021-04-08 17:34:36 +0600

    Merge remote-tracking branch 'origin/master'

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(
         primaryColor: Colors.blue,
       ),
